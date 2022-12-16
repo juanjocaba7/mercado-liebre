b3c8e474f30c522cdaa719d8c0dc4dbc7ceca284
juanjocaba7 <juanjocabanellas@gmail.com>
2022-12-15 21:31:46 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+
+const path = require('path');
+const app = express();
+
+const publicPath= path.resolve(__dirname,'../public')
+app.use(express.static(publicPath));
+
+const port=process.env.PORT || 3030;
+app.listen(port,() =>
+    console.log('Servidor corriendo en http://localhost:' + port)
+)
+
+
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get("/register", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+app.get("/login", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
